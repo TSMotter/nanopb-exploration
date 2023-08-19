bfd67d5626ec8b86aed9d8dd6d4ea311841bcc87
Giuliano Motter <mottergiuliano@gmail.com>
2023-08-19 17:38:28 -0300

    Edit exampleE so that I can have access to the samples contents at the same level where I call the 'decode' method. This is done through the usage of a ringbuffer;

--- exampleE/main.c
+++ exampleE/main.c
@@ -21,6 +21,8 @@ Example of samples:
 #define COMMON_BUFFER_SIZE 512
 #define WRITE_TO_FILE
 
+#define DECODED_SAMPLES_BUFFER_SIZE (5 * sizeof(Batch_Sample))
+
 /* clang-format off */
 static bool custom_ostream_callback(pb_ostream_t *stream, const pb_byte_t *buf, size_t count);
 static bool custom_repeated_encoding_callback(pb_ostream_t *stream, const pb_field_iter_t *field, void *const *arg);
@@ -32,23 +34,27 @@ static bool custom_repeated_decoding_callback(pb_istream_t *stream, const pb_fie
 static uint8_t    common_buffer[COMMON_BUFFER_SIZE];
 static RINGBUFF_T common_rb;
 
+static uint8_t    decoded_samples_buffer[DECODED_SAMPLES_BUFFER_SIZE];
+static RINGBUFF_T decoded_samples_rb;
 
 int main()
 {
     RingBuffer_Init(&common_rb, common_buffer, sizeof(uint8_t), COMMON_BUFFER_SIZE);
+    RingBuffer_Init(&decoded_samples_rb, decoded_samples_buffer, sizeof(Batch_Sample),
+                    DECODED_SAMPLES_BUFFER_SIZE);
 
     pb_ostream_t oStream = {&custom_ostream_callback, (void *) &common_rb, COMMON_BUFFER_SIZE, 0};
 
-    Batch_Sample samples[2] = {{.name      = "channel_2000_200_1",
-                                .frequency = 200,
-                                .time      = 0.051526546478271484,
-                                .value     = 0.31812476943300294},
-                               {.name      = "channel_2000_200_1",
-                                .frequency = 200,
-                                .time      = 0.056981563568115234,
-                                .value     = 0.3504258283522246}};
+    Batch_Sample samples_e[2] = {{.name      = "channel_2000_200_1",
+                                  .frequency = 200,
+                                  .time      = 0.051526546478271484,
+                                  .value     = 0.31812476943300294},
+                                 {.name      = "channel_2000_200_1",
+                                  .frequency = 200,
+                                  .time      = 0.056981563568115234,
+                                  .value     = 0.3504258283522246}};
 
-    Batch batch_e = {.items.arg          = samples,
+    Batch batch_e = {.items.arg          = samples_e,
                      .items.funcs.encode = &custom_repeated_encoding_callback};
 
     if (!pb_encode(&oStream, Batch_fields, &batch_e))
@@ -76,13 +82,26 @@ int main()
     pb_istream_t iStream = {&custom_istream_callback, (void *) &common_rb, COMMON_BUFFER_SIZE, 0};
 
     /* Allocate space for the decoded message. */
-    Batch batch_d = {.items.arg = NULL, .items.funcs.decode = &custom_repeated_decoding_callback};
+    Batch batch_d = {.items.arg          = &decoded_samples_rb,
+                     .items.funcs.decode = &custom_repeated_decoding_callback};
 
     /* Now we are ready to decode the message. */
     if (!pb_decode(&iStream, Batch_fields, &batch_d))
     {
         printf("Decoding failed: %s\n", PB_GET_ERROR(&iStream));
         return 1;
+    }
+
+    while (RingBuffer_GetCount(&decoded_samples_rb))
+    {
+        Batch_Sample sample_d = {};
+        RingBuffer_Pop(&decoded_samples_rb, &sample_d);
+        printf("~~~~~~~~~~~~~~~\n");
+        printf("sample_d.name: %s\n", sample_d.name);
+        printf("sample_d.frequency: %d\n", sample_d.frequency);
+        printf("sample_d.time: %f\n", sample_d.time);
+        printf("sample_d.value: %f\n", sample_d.value);
+        printf("~~~~~~~~~~~~~~~\n");
     }
 }
 
@@ -143,6 +162,8 @@ static bool custom_repeated_decoding_callback(pb_istream_t *stream, const pb_fie
 {
     printf("custom_repeated_decoding_callback!-> tag:%d\n", field->tag);
 
+    RINGBUFF_T *rb = (RINGBUFF_T *) *arg;
+
     if (stream != NULL && field->tag == Batch_items_tag)
     {
         Batch_Sample sample_d = Batch_Sample_init_zero;
@@ -153,10 +174,7 @@ static bool custom_repeated_decoding_callback(pb_istream_t *stream, const pb_fie
             return 1;
         }
 
-        printf("sample_d.name: %s\n", sample_d.name);
-        printf("sample_d.frequency: %d\n", sample_d.frequency);
-        printf("sample_d.time: %f\n", sample_d.time);
-        printf("sample_d.value: %f\n", sample_d.value);
+        RingBuffer_Insert(rb, &sample_d);
     }
 
     return true;
